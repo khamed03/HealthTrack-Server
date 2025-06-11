ee4868c65b72b4e36337185ba84edddd8c0a16f9
SK0603 <khalid.hamid2003@gmail.com~>
2025-06-11 11:06:06 +0300

    Test: Allow all origins to confirm CORS behavior

--- server.js
+++ server.js
@@ -11,18 +11,19 @@ dotenv.config();
 
 const app = express();
 const allowedOrigins = ["https://healthtrack-client-production.up.railway.app"];
+app.use(cors());
 
-app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"],
-}));
+// app.use(cors({
+//   origin: function (origin, callback) {
+//     if (!origin || allowedOrigins.includes(origin)) {
+//       callback(null, true);
+//     } else {
+//       callback(new Error("Not allowed by CORS"));
+//     }
+//   },
+//   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+//   allowedHeaders: ["Content-Type", "Authorization"],
+// }));
 
 
 //middleware
